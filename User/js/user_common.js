7aa327d57223130a736317d2f2e0c1f114fc09f9
minfei-minfei <minfei-minfei@139.com>
2017-09-02 17:02:48 +0800

    老年护工项目

--- User/js/user_common.js
+++ User/js/user_common.js
@@ -9,15 +9,24 @@ $(function(){
     $input.on("focus",function(){
         //获得焦点的时候，输入框是默认值，表示没有输入值，则设空
         if(this.value==this.defaultValue){
-            //this.value="";效率更高
-            $(this).val("");
+            this.value="";
         }
+        $(this).css("color","inherit");
     });
     $input.on("blur",function(){
         //失去焦点的时候，输入框为空，表示没有输入值，则重新设为默认值
         if(this.value==""){
             this.value = this.defaultValue;
+            $(this).css("color","lightgray");
+        }else{
+            $(this).css("color","inherit");
         }
     });
-
+    /*返回上一页*/
+    $(function(){
+        var $back = $(".glyphicon-chevron-left");
+        $back.on("click",function(){
+            history.back();
+        });
+    });
 });
